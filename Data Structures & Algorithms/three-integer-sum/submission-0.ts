840317e7e18b361ea0f8bec8e65ad8ca1000a682
Sebbak <145860553+SEBBAK-Mehdi-2326098aa@users.noreply.github.com>
2026-06-04 19:39:46 +0200

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        let result: number[][] = [];
+
+        nums = nums.sort((a, b) => a - b);
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+            let left = i + 1;
+            let right = nums.length - 1;
+            while (left < right) {
+                let addition = nums[i] + nums[left] + nums[right];
+                if (addition == 0) {
+                    result.push([nums[i], nums[left], nums[right]]);
+                    while (nums[left + 1] == nums[left]) {
+                        left++;
+                    }
+                    while (nums[right - 1] == nums[right]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (addition > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
